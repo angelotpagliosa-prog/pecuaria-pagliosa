2ceeadf105aefe55fef298dd6381d9c8860677dd
Angelo Pagliosa <angelotpagliosa@gmail.com>
2026-05-10 15:29:42 -0300

    fix: forcar novo hash de bundle no Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      }
+    }
+  },
+  define: {
+    __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
+  },
 })
